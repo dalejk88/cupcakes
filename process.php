f6398810432d8f606a98a7bcf8ffeea04666d6fc
Dale Kanikkeberg <kanikkeberg.dale@student.greenriver.edu>
2023-01-05 17:34:03 -0500

    added process page to print out what was ordered

---
+++ process.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Process</title>
+</head>
+<body>
+<?php
+    echo 'Thank you, '.$_POST['name'].', for your order!<br><br>';
+
+    echo 'Order Summary:';
+    if (isset($_POST['flavor'])) {
+        echo '<ul>';
+        foreach ($_POST['flavor'] as $item) {
+            echo '<li>' . $item . '</li>';
+        }
+        echo '</ul><br>';
+    }
+
+    echo 'Order Total: $';
+    echo ((double)sizeof($_POST['flavor']) * 3.50);
+?>
+</body>
+</html>
