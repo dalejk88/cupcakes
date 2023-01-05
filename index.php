10dd8ace5bc813db6e7565528745ff1b6bd46e9f
Dale Kanikkeberg <kanikkeberg.dale@student.greenriver.edu>
2023-01-05 17:14:01 -0500

    Create index page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cupcakes</title>
+</head>
+<body>
+    <form>
+    <h1>Cupcake Fundraiser</h1>
+        <label>Your name:<br><input type="text"></label>
+        <p>Cupcake flavors:</p>
+        <label><input type="checkbox" value="grasshopper" name="flavor[]">The Grasshopper</label><br>
+        <label><input type="checkbox" value="whiskey" name="flavor[]">Whiskey Maple Bacon</label><br>
+        <label><input type="checkbox" value="carrot" name="flavor[]">Carrot Walnut</label><br>
+        <label><input type="checkbox" value="caramel" name="flavor[]">Salted Caramel Cupcake</label><br>
+        <label><input type="checkbox" value="velvet" name="flavor[]">Red Velvet</label><br>
+        <label><input type="checkbox" value="lemon" name="flavor[]">Lemon Drop</label><br>
+        <label><input type="checkbox" value="tiramisu" name="flavor[]">Tiramisu</label><br>
+        <input type="submit" value="Order">
+    </form>
+</body>
+</html>
